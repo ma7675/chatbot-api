c33a5ffad999ea995af04a88155cbcb838c9e69c
MHJ <493712079@qq.com>
2024-04-01 23:03:22 +0800

    tijiao

---
+++ chatbot-api-common/src/main/java/cn/mahao/ai/App.java
@@ -0,0 +1,13 @@
+package cn.mahao.ai;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
